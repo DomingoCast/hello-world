5816687291b47e5953a38070c0ea6a0d0da1d32d
Pedro Larrosa <larrosacasellespedro@gmail.com>
2020-10-22 14:37:03 +0200

    Pedroañadimiento

---
+++ Developer.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Domingocrack
+{
+	public static void Main()
+	{
+		Console.Write("Dime tu nombre pirata:");
+		string name=Console.ReadLine();
+		
+		if((name=="Domingo") || (name=="domingo"))
+		{
+			Console.WriteLine("HELLO DEVELOPER!! :D");
+		}
+		else
+		{
+			Console.WriteLine("Hola desarrollador :c");
+		}
+	}
+}
